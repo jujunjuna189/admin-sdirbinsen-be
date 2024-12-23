e9a7380436350efb57847d199e54ef826d87cf64
Jujun Juna189 <53271616+jujunjuna189@users.noreply.github.com>
2024-12-24 01:41:37 +0700

    Update

--- app/Http/Controllers/Api/V1/Admin/RiwayatKeluargaController.php
+++ app/Http/Controllers/Api/V1/Admin/RiwayatKeluargaController.php
@@ -67,12 +67,8 @@ class RiwayatKeluargaController extends Controller
         try {
             $validator = Validator::make($request->all(), [
                 'personil_id' => 'required|integer',
-                'alamat_tinggal' => 'required',
-                'nomor_hp' => 'required',
                 'nama_ayah' => 'required',
                 'nama_ibu' => 'required',
-                'alamat_orang_tua' => 'required',
-                'nama_istri_atau_suami' => 'required',
                 'jumlah_anak' => 'nullable',
                 'status' => 'required',
             ], [
@@ -83,7 +79,6 @@ class RiwayatKeluargaController extends Controller
                 'nama_ayah.required' => "Nama Ayah wajib diisi!",
                 'nama_ibu.required' => "Nama Ibu wajib diisi!",
                 'alamat_orang_tua.required' => "Alamat Orang tua wajib diisi!",
-                'nama_istri_atau_suami.required' => "Nama Istri atau Suami wajib diisi!",
                 'status.required' => "Status wajib diisi!"
             ]);
 
